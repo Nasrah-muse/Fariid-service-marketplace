e05fd074a9596a8ecba1aeab6531518979adbe04
Nasrah-muse <nazronafta143@gmail.com>
2025-04-12 17:20:42 +0300

    feat: add pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { Route, Routes } from "react-router"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomePage from "./pages/HomePage"
+import ServicesPage from "./pages/ServicesPage"
+import CategoriesPage from "./pages/CategoriesPage"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+
  
 function App() {
  
   return (
     <div>
       {/* Header */}
+      <Header/>
       <main>
      {/* routes */}
+     <Routes>
+     <Route path="/" element={<HomePage/>}/>
+     <Route path="/services" element={<ServicesPage/>}/>
+     <Route path="/categories" element={<CategoriesPage/>}/>
+     <Route path="/contact" element={<ContactPage/>}/>
+     <Route path="/about" element={<AboutPage/>}/>
+     </Routes>
 
       </main>
       {/* Footer */}
+      <Footer/>
     </div>
  
   )
