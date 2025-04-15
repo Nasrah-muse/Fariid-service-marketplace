b9f14887cbc297e26b8cfb795ff9d5e23dd2922e
Nasrah-muse <nazronafta143@gmail.com>
2025-04-15 21:12:44 +0300

    feat(profile): add container and card of profile with theme

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,17 @@
+
+import { useTheme } from "../contexts/ThemeContext";
+
 const ProfilePage = () => {
+    const { theme } = useTheme();
+
   return (
-    <div>ProfilePage</div>
+    <div className={`min-h-screen ${theme === 'dark' ? 'bg-indigo-900' : 'bg-gray-50'} py-12 px-4 sm:px-6 lg:px-8`}>
+              <div className="max-w-3xl mx-auto">
+              <div className={`${theme === 'dark' ? 'bg-indigo-800' : 'bg-white'} shadow-xl rounded-lg overflow-hidden`}>
+
+                </div>
+                </div>
+</div>
   )
 }
 
