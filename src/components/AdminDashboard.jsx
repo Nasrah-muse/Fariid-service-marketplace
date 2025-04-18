3deaaf5a860fc9d67ef57ad4f04403550b2c3e83
Nasrah-muse <nazronafta143@gmail.com>
2025-04-18 21:43:59 +0300

    feat(admin): add sidebar with title

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -5,6 +5,12 @@ const AdminDashboard = () => {
     const {theme} = useTheme()
   return (
     <div className={`min-h-screen ${theme === 'dark' ? 'bg-indigo-900' : 'bg-gray-100'} mt-20`}>
+        
+        {/* Sidebar */}
+        <div className={`w-64 min-h-screen p-4 ${theme === 'dark' ? 'bg-indigo-800' : 'bg-white'} shadow`}>
+          <h2 className="text-2xl font-bold mb-8">Admin Dashboard</h2>
+
+    </div>
     </div>
   )
 }
