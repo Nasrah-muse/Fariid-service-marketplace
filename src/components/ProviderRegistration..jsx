1be93052efec5ab89bbb7f4226db70788dc5bdb9
Nasrah-muse <nazronafta143@gmail.com>
2025-04-21 15:15:33 +0300

    feat(provider): add Provider Regsitration container with title based on theme

--- src/components/ProviderRegistration..jsx
+++ src/components/ProviderRegistration..jsx
@@ -1,7 +1,16 @@
+import { useTheme } from "../contexts/ThemeContext"
+
   
  const ProviderRegistration = () => {
+  const {theme } = useTheme()
    return (
-     <div>ProviderRegistration.</div>
+     <div className={`min-h-screen p-6 mt-16 ${theme === 'dark' ? 'bg-indigo-800 text-gray-100' : 'bg-sky-200 text-indigo-900'}`}>
+       <div className={`max-w-4xl mx-auto p-6 rounded-lg ${theme === 'dark' ? 'bg-indigo-600 shadow-xl' : 'bg-white shadow-md'}`}>
+            <h2 className={`text-2xl font-bold mb-6 text-center ${theme === 'dark' ? 'text-white' : 'text-indigo-900'}`}>
+              Service Registration
+        </h2>
+       </div>
+       </div>
    )
  }
  
