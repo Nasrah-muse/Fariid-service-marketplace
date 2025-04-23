30592c32dc931300f871cf5201a97a4bd249cbea
Nasrah-muse <nazronafta143@gmail.com>
2025-04-23 12:20:17 +0300

    feat: display the services that provider registered with their status in table body

--- src/components/AdminServiceList.jsx
+++ src/components/AdminServiceList.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import { useTheme } from "../contexts/ThemeContext"
+import supabase from "../lib/supabase";
 
  
 export default function AdminServiceList() {
  const {theme} = useTheme()
+ const [services, setServices] = useState([])
+  useEffect(() => {
+    const fetchServices = async () => {
+      const { data, error } = await supabase
+      .from("services")
+      .select(`
+        *,
+        category:categories (
+          name
+        )
+      `)
+      if (error) console.error("Error fetching services:", error)
+      else setServices(data)
+    };
+
+    fetchServices()
+  }, [])
+
+
   return (
      <div className="p-4">
  <div className="overflow-x-auto">
@@ -16,7 +37,35 @@ export default function AdminServiceList() {
       </tr>
     </thead>
     <tbody>
-        no services
+    {services.map(service => (
+              <tr key={service.id}>
+                <td className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>
+                  {service.title}
+                </td>
+                <td className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>
+                    {service.category?.name || 'N/A'}</td>
+                <td className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>
+                  <span className={`inline-block px-2 py-1 rounded-full text-sm ${
+                    service.status === 'approved' ? 'bg-green-100 text-green-800' :
+                    service.status === 'pending' ? 'bg-yellow-500 text-yellow-800' :
+                    'bg-red-100 text-red-800'}`}>
+                    {service.status.charAt(0).toUpperCase() + service.status.slice(1)}
+                  </span>
+                </td>
+                <td className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>
+                  <button
+                    className="bg-green-500 text-white px-3 py-1 mr-2 rounded"
+                   >
+                    Approve
+                  </button>
+                  <button
+                    className="bg-red-500 text-white px-3 py-1 rounded"
+                   >
+                    Reject
+                  </button>
+                </td>
+              </tr>
+            ))}
     </tbody>
        
   </table>
