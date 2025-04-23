750df60c25bd9fce5b90121f9f5f0eba16900a20
Nasrah-muse <nazronafta143@gmail.com>
2025-04-23 12:10:17 +0300

    feat: Ad service list in approve providers tab in admin dashboard and add table with head in service list component based on theme

--- src/components/AdminServiceList.jsx
+++ src/components/AdminServiceList.jsx
@@ -1,9 +1,27 @@
+import { useTheme } from "../contexts/ThemeContext"
+
  
 export default function AdminServiceList() {
- 
+ const {theme} = useTheme()
   return (
-     <div>
+     <div className="p-4">
+ <div className="overflow-x-auto">
+  <table className="min-w-full table-auto">
+    <thead>
+      <tr>
+        <th className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>Title</th>
+        <th className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>Category</th>
+        <th className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>Status</th>
+        <th className={`border p-2 ${theme === 'dark'? 'border-gray-100 text-sky-200': 'border-indigo-600 text-indigo-900'}`}>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+        no services
+    </tbody>
+       
+  </table>
 
+     </div>
      </div>
   )
 }
