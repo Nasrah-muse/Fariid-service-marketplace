8e0214b9160b5a1f1ee86038b76bdeef4ab8d22f
Nasrah-muse <nazronafta143@gmail.com>
2025-04-25 18:30:43 +0300

    feat(About): Add our vison section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -13,6 +13,32 @@ const AboutPage = () => {
           </h1>
         </div>
         </div>
+         <section className="w-full">
+          <div className="max-w-4xl mx-auto  px-4 sm:px-6 lg:px-8 py-12">
+            <h1 className="text-center text-3xl font-bold mb-4">Our Vision</h1>
+          <div className="grid md:grid-cols-3 gap-8">
+            <div className={`flex flex-col items-center justify-center ${theme === 'dark'? 'bg-indigo-700': 'bg-white'} shadow-lg rounded p-4 gap-1`}>
+            <h2 className="text-2xl font-bold ">01</h2>
+              <div className="text-center font-semibold">
+              To connect skilled professionals with clients who need their services, making work easier for everyone
+              </div>
+            </div>
+            <div className={`flex flex-col items-center justify-center ${theme === 'dark'? 'bg-indigo-700': 'bg-white'} shadow-lg rounded p-4 gap-1`}>
+            <h2 className="text-2xl font-bold ">02</h2>
+              <div className="text-center font-semibold">
+              A world where finding the right service is quick, easy, and fair for both providers and customers.
+              </div>
+            </div>
+            <div className={`flex flex-col items-center justify-center ${theme === 'dark'? 'bg-indigo-700': 'bg-white'} shadow-lg rounded p-4 gap-1`}>
+            <h2 className="text-2xl font-bold ">03</h2>
+              <div className="text-center font-semibold">
+              Helping people grow their businesses and careers through trusted service partnerships
+              </div>
+            </div>
+          </div>
+          </div>
+        </section>
+
     </div>
   )
 }
