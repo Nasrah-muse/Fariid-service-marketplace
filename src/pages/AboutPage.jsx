28ba08f19c331406827f34e5db5b9f779067ef15
Nasrah-muse <nazronafta143@gmail.com>
2025-04-25 19:05:35 +0300

    update(About): hero section theme

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,9 +6,9 @@ const AboutPage = () => {
  
   return (
     <div className={`min-h-screen mt-12 ${theme === 'dark' ? 'bg-indigo-900 text-white' : 'bg-gray-50 text-indigo-900'}`}>
-      <div className={`max-w-full h-96  px-4 sm:px-6 lg:px-8 py-12 bg-gradient-to-r ${theme === 'dark'? "from-sky-400 to-orange-600" :"from-indigo-600 to-sky-200"  } `}>
+      <div className={`max-w-full h-96  px-4 sm:px-6 lg:px-8 py-12 bg-gradient-to-r ${theme === 'light'? "from-indigo-900 to-orange-500" :"from-indigo-600 to-sky-200"  } `}>
          <div className="text-center mb-16">
-          <h1 className={`text-4xl md:text-5xl font-bold mb-6 ${theme === 'dark' ? 'text-white' : 'text-indigo-900'}`}>
+          <h1 className={`text-4xl md:text-5xl font-bold mb-6 ${theme === 'dark' ? 'text-white' : 'text-sky-200'}`}>
             About Us
           </h1>
         </div>
